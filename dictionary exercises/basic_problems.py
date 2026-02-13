fdc04d78476f17ebfbe7b698fa75df9cc076bdd1
Arvi-1nd <davidarvind44@gmail.com>
2026-02-13 12:12:49 +0530

    adding swap variables

--- dictionary exercises/basic_problems.py
+++ dictionary exercises/basic_problems.py
@@ -583,3 +583,25 @@ print(res)
 nest_dict = {key : {k:v} for key, (k,v) in zip(bel,ael.items())}
 
 print(nest_dict)
+
+
+# Swapping hierarchy in nested dictionaries
+
+test_dicty = {'Gfg': { 'a' : [1, 3], 'b' : [3, 6], 'c' : [6, 7, 8]},
+             'Best': { 'a' : [7, 9], 'b' : [5, 3, 2], 'd' : [0, 1, 0]}}
+
+
+ret = dict()
+
+for key, val in test_dicty.items():
+    for key_in, val_in in val.items():
+        if key_in not in ret:
+            temp = dict()
+        else:
+            temp = ret[key_in]
+        
+        temp[key] = val_in
+        ret[key_in] = temp
+        
+        
+print(f"{ret}")
